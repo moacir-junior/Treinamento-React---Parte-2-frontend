3af96d3ab47a677bb05e0620af71db9f4950c3cc
Moacir Zimermann Junior <moacirjrzimermann@gmail>
2021-04-11 22:45:38 -0300

    Request para API

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { IconButton } from '@material-ui/core';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
 import './Topbar.scss';
 
 function Topbar(props) {
-  const { title } = props;
-
+  const { title, showMenu, setShowMenu } = props;
+  
   return (
     <div className="topbar">
+      {!showMenu && <IconButton className="topbar__button" aria-label="upload picture" onClick={() => setShowMenu(true)}>
+        <ExpandMore />        
+      </IconButton>}
+      {showMenu && <IconButton className="topbar__button" aria-label="upload picture" onClick={() => setShowMenu(false)}>
+        <ExpandLess />
+      </IconButton>}
       <span className="topbar__title">{title}</span>
     </div>
   );
